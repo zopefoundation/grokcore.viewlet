000a1f0c0bf574ed054a0e4ddb76730e22f483bc
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2012-09-04 11:35:54 +0000

    Back to development: 1.12

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ tests_require = [
 
 setup(
     name='grokcore.viewlet',
-    version='1.11',
+    version='1.12.dev0',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
