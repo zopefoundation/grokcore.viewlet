60556c1e46c81c0bf02627ffd9cf481c1189c8d6
Michael Howitz <icemac@gmx.net>
2023-08-28 11:30:58 +0200

    Garden package.

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     name='grokcore.viewlet',
     version='4.1.dev0',
     author='Grok Team',
-    author_email='grok-dev@zope.org',
+    author_email='zope-dev@zope.dev',
     url='https://github.com/zopefoundation/grokcore.viewlet',
     download_url='https://pypi.org/project/grokcore.viewlet',
     description='Grok-like configuration for zope viewlets',
@@ -61,6 +61,7 @@ setup(
     namespace_packages=['grokcore'],
     include_package_data=True,
     zip_safe=False,
+    python_requires='>=3.7',
     install_requires=[
         'grokcore.component >= 2.5',
         'grokcore.security >= 1.6',
